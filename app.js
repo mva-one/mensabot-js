0b69ae052cda822c60e87df10350841eb3d132a8
mva-one <marcell@mva-one.de>
2022-06-09 00:00:03 +0200

    Does not work. Too tired to continue.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+require('console-stamp')(console, 'HH:MM:ss');
 
 // Read the JSON config file
 try {
@@ -10,14 +11,9 @@ try {
 }
 
 // First bot test
-const { Telegraf } = require('telegraf')
+const { Markup, Telegraf } = require('telegraf')
 
 const bot = new Telegraf(config.bot_token)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 
 bot.use(async (ctx, next) => {
     console.time(`Processing update ${ctx.update.update_id}`)
@@ -26,15 +22,50 @@ bot.use(async (ctx, next) => {
     console.timeEnd(`Processing update ${ctx.update.update_id}`)
 });
 
-function next () {
+async function next() {
     console.log('THIS IS NEXTT!!!')
 }
+
+
+function currentYearAndWeekNumber() {
+    var date = new Date();
+    var currentThursday = new Date(date.getTime() + (3 - ((date.getDay() + 6) % 7)) * 86400000);
+    var yearOfThursday = currentThursday.getFullYear();
+    var firstThursday = new Date(new Date(yearOfThursday, 0, 4).getTime() + (3 - ((new Date(yearOfThursday, 0, 4).getDay() + 6) % 7)) * 86400000);
+    return date.getFullYear() + '/' + Math.floor(1 + 0.5 + (currentThursday.getTime() - firstThursday.getTime()) / 86400000 / 7);
+}
+
+
+const mensa_keyboard = Markup.keyboard(['Heide', 'Weinberg', 'Harz', 'Francke', 'Alle 4'], { columns: 2 })
+
+bot.start((ctx) => {
+    return ctx.reply('Wähle die Mensa, um das Menü anzuzeigen.', mensa_keyboard)
+})
+
+bot.hears('Heide', (ctx) => {
+    fetch(config.api_url + currentYearAndWeekNumber())
+    .then(
+        console.log(response)
+    )
+    .catch()
+    return ctx.reply('Heidemenu')
+        /* .then((response) => {
+            // Do something with response
+            return ctx.reply('Menue' + response.9.dates.2022-06-07.3.name)
+        })
+        .catch(function (err) {
+            console.error("Unable to fetch -", err);
+            return ctx.reply('ERROR Menue fuer Heide')
+        })    */ 
+})
+
+
 
 
 bot.launch()
 
 
-console.log(config);
+console.log("---- BOT LAUNCHED ----   Let's go! Current week number is: " + currentYearAndWeekNumber());
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
